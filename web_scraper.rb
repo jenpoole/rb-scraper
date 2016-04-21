6ceef261cd8de3b7a653b44214cc8c07f632b478
Jen <contact.vandepool@gmail.com>
2016-04-21 19:36:27 +0000

    scrape to csv

--- web_scraper.rb
+++ web_scraper.rb
@@ -1,8 +1,31 @@
-require 'HTTParty'
-require 'Nokogiri'
-require 'JSON'
-require 'Pry'
+require 'httparty'
+require 'nokogiri'
+require 'json'
+require 'pry'
 require 'csv'
 
 # get request for url to be scraped
 page = HTTParty.get('https://newyork.craigslist.org/search/pet')
+
+# convert HTML string to Nokogiri object for data to be parsed
+parse_page = Nokogiri::HTML(page)
+
+# array to store each individual listing
+pets_array = []
+
+# Examine html content (in pry) using Nokogiri css method and element classes
+# parse_page.css('.content').css('.row').css('.hdrlnk')
+
+# parse the data
+# convert headline text of each listing to a string & push to array
+parse_page.css('.content').css('.row').css('.hdrlnk').map do |a|
+    post_name = a.text
+    pets_array.push(post_name)
+end
+
+# push array to CSV file
+CSV.open('pets.csv', 'w') do |csv|
+    csv << pets_array
+end
+
+# Pry.start(binding)
